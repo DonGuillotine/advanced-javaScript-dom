3fd1b36f61c828e9d08427387920eca969cfd80c
Donald Nwokoro <infect3dlab@gmail.com>
2023-07-25 13:35:52 +0100

    Feat ✨: Advanced DOM Manipulation: Keydown Event

--- script.js
+++ script.js
@@ -24,3 +24,10 @@ btnOpenModal.forEach(button => button.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+
+document.addEventListener('keydown', function(event){
+    if(event.key === 'Escape' && !modal.classList.contains('hidden')){
+        closeModal();
+    }
+});
