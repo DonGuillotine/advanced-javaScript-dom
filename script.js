5c4e4ecf8ac01a49cd45c2a924184467db656922
Donald Nwokoro <infect3dlab@gmail.com>
2023-07-26 13:03:41 +0100

    Feat ✨: Tabs Component

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const btnOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
 
 
 // *****************************************Modal Window**************************************
@@ -54,8 +57,30 @@ btnScrollTo.addEventListener('click', function(){
 
 document.querySelector('.nav__links').addEventListener('click', function(event){
     event.preventDefault();
+    // console.log(event.target);
     if(event.target.classList.contains('nav__link')){
-        const id = event.target.getAttribute('href');
-        document.querySelector(id).scrollIntoView({behavior: 'smooth'});
+        const identifier = event.target.getAttribute('href');
+        document.querySelector(identifier).scrollIntoView({behavior: 'smooth'});
     }
+});
+
+
+//******************************************* Tabbed Component ********************************/
+tabsContainer.addEventListener('click', function(event){
+    const clicked = event.target.closest('.operations__tab');
+    console.log(clicked);
+
+    // Guard Clause
+    // If 'operations__tab' is not clicked, the retuen statement immediately exit the event listener function
+    if(!clicked) return;
+
+    // Remove active classes
+    tabs.forEach(tabsButton => tabsButton.classList.remove('operations__tab--active'));
+
+    tabsContent.forEach(operationsContent => operationsContent.classList.remove('operations__content--active'));
+
+    // Activate content area
+    clicked.classList.add('operations__tab--active');
+
+    document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
 });
