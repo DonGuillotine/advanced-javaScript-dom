2b873442a956e4c966a3845188752da75886b377
Donald Nwokoro <infect3dlab@gmail.com>
2023-08-01 14:53:27 +0100

    Feat ✨: Slider Logic

--- script.js
+++ script.js
@@ -187,7 +187,7 @@ const ImgOptions = {
 
 const imgObserver = new IntersectionObserver(loadImg, ImgOptions);
 imgTargets.forEach(img => imgObserver.observe(img));
-It
+
 
 
 
@@ -213,3 +213,60 @@ const obsOptions = {
 
 // const observer = new IntersectionObserver(obsCallBack, obsOptions);
 // observer.observe(section1);
+
+
+const slider = function(){
+    const slides = document.querySelectorAll('.slide');
+    const btnLeft = document.querySelector('.slider__btn--left');
+    const btnRight = document.querySelector('.slider__btn--right');
+    const dotContainer = document.querySelector('.dots');
+
+    let currentSlide = 0;
+    const maximumSlide = slides.length;
+
+
+    const createDots = function(){
+        slides.forEach(function(_, i){
+            dotContainer.insertAdjacentHTML('beforeend', `<button class="dots__dot" data-slide="${i}"></button>`);
+        });
+    }
+
+    const activateDot = function(slide){
+        document.querySelectorAll('.dots__dot').forEach(dot => dot.classList.remove('dots__dot--active'));
+
+        document.querySelector(`dots__dot[data-slide="${slide}]`).classList.add('dots__dot--active');
+    }
+
+    const goToSlide = function(slide){
+        slides.forEach((slide, i) => slide.style.transform = `translateX(${100 * (i - slide)}%)`);
+    }
+
+    const nextSlide = function(){
+        if(currentSlide === maximumSlide - 1){
+            currentSlide = 0;
+        }
+        else{
+            currentSlide++;
+        }
+        goToSlide(currentSlide);
+        activateDot(currentSlide);
+    }
+
+    const previousSlide = function(){
+        if(currentSlide === 0){
+            currentSlide = maximumSlide - 1;
+        }
+        else{
+            currentSlide--;
+        }
+        goToSlide(currentSlide);
+        activateDot(currentSlide);
+    }
+
+    btnRight.addEventListener('click', nextSlide);
+    btnLeft.addEventListener('click', previousSlide);
+
+    createDots();
+}
+
+slider();
