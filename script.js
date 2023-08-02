3ec48b3587377c88957a43ed39808dd41c2494c5
Donald Nwokoro <infect3dlab@gmail.com>
2023-08-02 12:30:39 +0100

    Feat ✨: Completed Slider

--- script.js
+++ script.js
@@ -223,6 +223,7 @@ const slider = function(){
 
     let currentSlide = 0;
     const maximumSlide = slides.length;
+    console.log(maximumSlide);
 
 
     const createDots = function(){
@@ -234,11 +235,11 @@ const slider = function(){
     const activateDot = function(slide){
         document.querySelectorAll('.dots__dot').forEach(dot => dot.classList.remove('dots__dot--active'));
 
-        document.querySelector(`dots__dot[data-slide="${slide}]`).classList.add('dots__dot--active');
+        document.querySelector(`.dots__dot[data-slide="${slide}"]`).classList.add('dots__dot--active');
     }
 
     const goToSlide = function(slide){
-        slides.forEach((slide, i) => slide.style.transform = `translateX(${100 * (i - slide)}%)`);
+        slides.forEach((s, i) => s.style.transform = `translateX(${100 * (i - slide)}%)`);
     }
 
     const nextSlide = function(){
@@ -263,10 +264,29 @@ const slider = function(){
         activateDot(currentSlide);
     }
 
+    const init = function(){
+        goToSlide(0);
+        createDots();
+        activateDot(0);
+    }
+
+    init();
+
     btnRight.addEventListener('click', nextSlide);
     btnLeft.addEventListener('click', previousSlide);
 
-    createDots();
+    document.addEventListener('keydown', function(event){
+        if(event.key === 'ArrowLeft') previousSlide();
+        event.key === 'ArrowRight' && nextSlide();
+    });
+
+    dotContainer.addEventListener('click', function(event){
+        if(event.target.classList.contains('dots__dot')){
+            const slide = event.target.dataset.slide;
+            goToSlide(slide);
+            activateDot(slide);
+        }
+    })
 }
 
 slider();
